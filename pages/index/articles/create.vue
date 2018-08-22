b1f50e1820a400f6e939b2ce9c0621256191908a
Curtis McDonald <curtis@Macurtis.local>
2018-08-22 08:53:00 -0400

    Create article template for Mu

---
+++ pages/index/articles/create.vue
@@ -0,0 +1,30 @@
+<style lang="css">
+
+
+
+</style>
+
+<template lang="html">
+
+<div class="articles-create">
+    <form class="" @submit.prevent="createArticle()">
+        <input type="text" name="" value="">
+    </form>
+</div>
+
+</template>
+
+<script>
+
+export default {
+    data: (vueModule) => ({
+
+    }),
+    methods: {
+        createArticle: function() {
+
+        }
+    }
+}
+
+</script>
